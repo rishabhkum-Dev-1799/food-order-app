6f4183c17e19cac2cbc0614e3c8bf1c427d0a02b
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-05-14 14:13:43 +0530

    "featureMeals-Created Meal Components for the website"

---
+++ starting-project/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { DUMMY_MEALS } from './DummyData/dummyData';
+import styles from './Styles/AvailableMeals.module.css';
+const AvailableMeals = () => {
+    const meals = DUMMY_MEALS.map((meal) => <li>meal.name</li>)
+    return (
+        <>
+            <section className={styles.meals}>
+                <ul>
+                    {meals}
+                </ul>
+            </section>
+        </>
+    )
+};
+
+export default AvailableMeals;

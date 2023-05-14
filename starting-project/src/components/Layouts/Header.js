9f2beedd01e67cffffbbbad18cd7f6c81409d5d1
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-05-14 13:43:00 +0530

    "featureHeader-Cart button on Header Added"

--- starting-project/src/components/Layouts/Header.js
+++ starting-project/src/components/Layouts/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import mealsImage from '../../assets/meals.jpg';
 import styles from './Header.module.css';
+import HeaderButton from './HeaderButton';
 
 const Header = (props) => {
     return (
         <>
             <header className={styles.header}>
                 <h1>QuickMeals</h1>
-                <button>Cart</button>
+                <HeaderButton />
             </header>
             <div className={styles['main-image']}>
                 <img src={mealsImage} alt='A table with delicious foods' />

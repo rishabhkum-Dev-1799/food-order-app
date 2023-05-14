4831c1fcd183cf16b3428044f1d85587ec6bd91c
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-05-14 14:45:06 +0530

    "featureMeal-Added UI-Card Wrapper Component"

--- starting-project/src/components/Meals/AvailableMeals.js
+++ starting-project/src/components/Meals/AvailableMeals.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { DUMMY_MEALS } from './DummyData/dummyData';
+import Card from '../UI/Card/Card';
 import styles from './Styles/AvailableMeals.module.css';
 const AvailableMeals = () => {
     const meals = DUMMY_MEALS.map((meal) => <li key={meal.id}>{meal.name}</li>)
     return (
         <>
             <section className={styles.meals}>
-                <ul>
-                    {meals}
-                </ul>
+                <Card>
+                    <ul>
+                        {meals}
+                    </ul>
+                </Card>
             </section>
         </>
     )

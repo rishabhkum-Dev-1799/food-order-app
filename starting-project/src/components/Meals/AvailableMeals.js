01f2890b03b3b63bf85c089c977eb7ba9f91b6f6
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-05-14 14:37:29 +0530

    "fix-AvaiableMeals Name not displaying"

--- starting-project/src/components/Meals/AvailableMeals.js
+++ starting-project/src/components/Meals/AvailableMeals.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { DUMMY_MEALS } from './DummyData/dummyData';
 import styles from './Styles/AvailableMeals.module.css';
 const AvailableMeals = () => {
-    const meals = DUMMY_MEALS.map((meal) => <li>meal.name</li>)
+    const meals = DUMMY_MEALS.map((meal) => <li key={meal.id}>{meal.name}</li>)
     return (
         <>
             <section className={styles.meals}>

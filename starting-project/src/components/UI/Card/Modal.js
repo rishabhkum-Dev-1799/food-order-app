a93f9eb93cf5d91671c02ce65af37ed47fb4c534
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-05-15 22:24:24 +0530

    "featureCart-Created Cart component for the site"

---
+++ starting-project/src/components/UI/Card/Modal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import styles from './Styles/Modal.module.css';
+
+const Backdrop = () => {
+    return (
+        <div className={styles.backdrop}></div>
+    )
+}
+const ModalOverlay = (props) => {
+    return (
+        <>
+            <div className={styles.modal}>
+                <div className={styles.content}>{props.children}</div>
+            </div>
+        </>
+    )
+}
+const portalElement = document.querySelector('#overlays')
+const Modal = (props) => {
+    return (
+        <>
+            {ReactDOM.createPortal(<Backdrop />, portalElement)}
+            {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+        </>
+    )
+}
+
+export default Modal;
